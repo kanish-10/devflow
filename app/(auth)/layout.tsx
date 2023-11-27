c7cea40109c2f7058ec4d8f795cd320e806e081f
Kanish Chheda <kanishchheda@gmail.com>
2023-11-27 16:58:13 +0530

    routing and auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../globals.css";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      {children}
+    </main>
+  );
+}
