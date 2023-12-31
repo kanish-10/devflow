3afbb1dc7b7c3ca3561d77f3d12e9872332741bc
Kanish Chheda <kanishchheda@gmail.com>
2023-12-31 13:08:14 +0530

    tags page

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -2,7 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDb } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -20,6 +21,17 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { name: "tag2", _id: "2" },
       { name: "tag3", _id: "3" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDb();
+    const tags = await Tag.find({});
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
