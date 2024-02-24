23460374819c3213d6e6e2f573a3cf3ffa26dc1f
Kanish Chheda <kanishchheda@gmail.com>
2024-02-25 00:48:01 +0530

    Authentication completed with clerk

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJS I will master you.</h1>
-      <h2 className="h2-bold">NextJS I will master you.</h2>
-      <h3 className="h3-bold">NextJS I will master you.</h3>
-    </div>
-  );
-}
