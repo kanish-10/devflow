8810dbdfe1f19e25b60749095de8613705908ed4
Kanish Chheda <kanishchheda@gmail.com>
2024-03-02 15:50:09 +0530

    Question Card UI completed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,6 +4,31 @@ import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import Filter from "@/components/shared/Filter";
 import { HomePageFilters } from "@/constants/filters";
 import HomeFilters from "@/components/shared/HomeFilters";
+import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/cards/QuestionCard";
+
+const questions = [
+  {
+    _id: "1",
+    title: "Cascading Deletes in SQLAlchemy?",
+    tags: [{ _id: "1", name: "python" }],
+    author: { _id: "1", name: "John Doe", picture: "/assets/icons/avatar.svg" },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: "2",
+    title: "How to center a div",
+    tags: [{ _id: "1", name: "javascript" }],
+    author: { _id: "1", name: "John Doe", picture: "/assets/icons/avatar.svg" },
+    upvotes: 1000000,
+    views: 10,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+];
 
 export default function Home() {
   return (
@@ -31,6 +56,30 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a question"
+          />
+        )}
+      </div>
     </>
   );
 }
