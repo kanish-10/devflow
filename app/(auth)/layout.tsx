6b997025c4e6462742496b5d84ee6da1931a0d32
Kanish Chheda <kanishchheda@gmail.com>
2024-03-09 21:04:19 +0530

    ...

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center">
+    <main className="flex min-h-screen w-full items-center justify-center bg-auth-light bg-cover bg-center bg-no-repeat dark:bg-auth-dark">
       {children}
     </main>
   );
